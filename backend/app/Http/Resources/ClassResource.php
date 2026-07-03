43bfbf57a54ad724dc5e7a4c75c1a5d4d5d82a0e
aegold <168075155+aegold@users.noreply.github.com>
2026-07-03 14:31:36 +0700

    feat: implement practice management service, student assessment logic, and base question seeders

--- backend/app/Http/Resources/ClassResource.php
+++ backend/app/Http/Resources/ClassResource.php
@@ -17,6 +17,7 @@ class ClassResource extends JsonResource
             'created_by' => $this->created_by,
             'creator' => new UserResource($this->whenLoaded('creator')),
             'teacher' => new UserResource($this->teacher), // append attribute
+            'users' => UserResource::collection($this->whenLoaded('users')),
             'students' => UserResource::collection($this->whenLoaded('students')),
             'teachers' => UserResource::collection($this->whenLoaded('teachers')),
             'created_at' => $this->created_at,
